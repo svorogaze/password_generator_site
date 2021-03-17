82f351e823e99b4e5b8476d39e4d4932e91b0573
svorogaze <68436858+svorogaze@users.noreply.github.com>
2021-03-17 20:38:40 +0300

    Improved checking for valid arguments

--- password_gen/views.py
+++ password_gen/views.py
@@ -4,12 +4,17 @@ from .modules import password_generator
 
 def home(request):
     if request.method == 'POST':
+
         lowercase_letters = request.POST.get('lowercase letters') == 'on'
         uppercase_letters = request.POST.get('uppercase letters') == 'on'
         numbers = request.POST.get('numbers') == 'on'
         special_chars = request.POST.get('special chars') == 'on'
+
+        if not request.POST.get('length of password').isdigit():
+            return render(request, 'error.html')
+
         len_of_pass = int(request.POST.get('length of password'))
-        if len_of_pass < 17 and any([lowercase_letters, uppercase_letters, numbers, special_chars]):
+        if 0 < len_of_pass < 17 and any([lowercase_letters, uppercase_letters, numbers, special_chars]):
             password = password_generator.generate_password(lowercase_letters, uppercase_letters, numbers,
                                                             special_chars,
                                                             len_of_pass)
